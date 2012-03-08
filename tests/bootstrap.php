2a30f229ea7995e3979340fe62f40f5c137920dc
Rafał Filipek <rafal.filipek@gmail.com>
2012-03-09 00:27:01 +0100

    Cleanup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,19 +3,3 @@
 $loader = require __DIR__.'/../vendor/.composer/autoload.php';
 
 $loader->add('Spriter', __DIR__.'/../src');
-
-/*
-
-spl_autoload_register(function($class)
-{
-	$path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-	foreach (array('tests') as $dirPrefix) {
-		$file = __DIR__.'/../'.$dirPrefix.'/'.$path.'.php';
-		if (file_exists($file)) {
-			require_once $file;
-			return true;
-		}
-	}
-});
-
-*/
